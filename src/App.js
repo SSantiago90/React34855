dbe406012ba9ca20e17538f0201bca6b7420922a
ssantiago90 <salkin.santiago@gmail.com>
2022-12-12 22:35:03 -0300

    codigo clase 3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  
+  const title = "¡¡¡Bienvenidos a React!!!!!!!";
+
+  const producto = {
+    precio: 500,
+    nombre: "Pantalón",
+    descripcion: "La remera de Coder",
+  };
+
+  const cardProduct = (
+    <div>
+      <h3>{producto.nombre}</h3>
+      <br />
+      <h3>$ {producto.precio}</h3>
+    </div>
+  );
+
+  function hacerAlgo() {
+    alert("Hola");
+  }
+
+  const miEstilo = {
+    backgroundColor: "pink",
+    margin: "30px",
+    padding: "50px",
+    textAlign: "center",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Bienvenidos a Coderhouse!
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {title}
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          ></a>
+          {cardProduct}
+        </header>
+      </div>
+      <div style={miEstilo} className="catalogo">
+        {cardProduct}
+      </div>
+      <button onClick={hacerAlgo}>Click me</button>
     </div>
   );
 }
